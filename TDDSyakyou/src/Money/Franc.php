7043f8274d394ec3607544ef6d4049596768bffd
rails -v <arimoto@n-di.co.jp>
2017-11-04 16:47:24 +0900

    58ページ

--- TDDSyakyou/src/Money/Franc.php
+++ TDDSyakyou/src/Money/Franc.php
@@ -3,14 +3,19 @@ namespace MyApp\Money;
 
 class Franc extends Money
 {
+
+
     public function __construct(int $amount)
     {
         $this->amount = $amount;
+        $this->currency = 'CHF';
     }
 
     public function times(int $multipier): Money
     {
         return new Franc($this->amount * $multipier);
     }
+
+
 
 }
